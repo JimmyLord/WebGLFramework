784caa12bc29742edab8cff0d62b31853b10f9e5
Jimmy Lord <jimmylord@gmail.com>
2020-03-03 19:40:43 -0500

    Drew more symbols for imgui font.

--- js/frameworkmain.js
+++ js/frameworkmain.js
@@ -136,6 +136,8 @@ class FrameworkMain
     {
         this.imgui.window( "ImGui Test" );
         //this.imgui.windows["ImGui Test"].size.setF32( 143, 120 );
+        this.imgui.text( "!\"#$%&'()*+,-./" );
+        this.imgui.text( "<=>?@" );
         this.imgui.text( "Te" );
         this.imgui.sameLine();
         this.imgui.text( "st" );
